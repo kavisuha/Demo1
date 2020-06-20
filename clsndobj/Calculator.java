e958456f00007a621b1f87e46dd3270db352a8c5
kavisuha <66821996+kavisuha@users.noreply.github.com>
2020-06-20 13:08:20 +0530

    Add files via upload

---
+++ clsndobj/Calculator.java
@@ -0,0 +1,23 @@
+public class Calculator {
+
+    public static void main(String[] args) 
+    {
+    	
+    System.out.println(Calci.powerDouble(85.0, 2));
+    System.out.println(Calci.powerInt(85,3));
+    }
+
+}
+
+class Calci
+{
+    static double powerInt(int num1,int num2)
+    {
+        return Math.pow(num1,num2);
+    }
+static double powerDouble(double num1,int num2)
+{
+    return Math.pow(num1,num2);
+}
+}
+
